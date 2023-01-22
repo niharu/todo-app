d3433718439977a1d386deab0f985bcffb2dd7cf
Ryo Nishimura <wv.nsmr@gmail.com>
2023-01-22 12:39:05 +0900

    fisrt

---
+++ src/components/App.js
@@ -0,0 +1,67 @@
+import React, { useRef } from "react";
+
+import { Container } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+
+import { TodoTitle } from "./TodoTitle";
+import { TodoAdd } from "./TodoAdd";
+import { TodoList } from "./TodoList";
+import { useTodo } from "../hooks/useTodo";
+
+function App() {
+  const {
+    todoList,
+    addTodoListItem,
+    toggleTodoListItemStatus,
+    deleteTodoListItem,
+  } = useTodo();
+
+  const inputEl = useRef(null);
+
+  const handleAddTodoListItem = () => {
+    if (inputEl.current.value === "") return;
+
+    addTodoListItem(inputEl.current.value);
+    inputEl.current.value = "";
+  };
+
+  const inCompletedList = todoList.filter((todo) => {
+    return !todo.done;
+  });
+
+  const completedList = todoList.filter((todo) => {
+    return todo.done;
+  });
+
+  return (
+    <Container centerContent p={{ base: "4", md: "6" }} maxWidth="3xl">
+      <TodoTitle title="TODO進捗管理" fontSize="2xl" />
+
+      <TodoAdd
+        placeholder="ADD TODO"
+        leftIcon={<AddIcon />}
+        buttonText="TODOを追加"
+        inputEl={inputEl}
+        handleAddTodoListItem={handleAddTodoListItem}
+      />
+
+      <TodoList
+        todoList={inCompletedList}
+        toggleTodoListItemStatus={toggleTodoListItemStatus}
+        deleteTodoListItem={deleteTodoListItem}
+        title="未完了TODOリスト"
+        fontSize="xl"
+      />
+
+      <TodoList
+        todoList={completedList}
+        toggleTodoListItemStatus={toggleTodoListItemStatus}
+        deleteTodoListItem={deleteTodoListItem}
+        title="完了TODOリスト"
+        fontSize="xl"
+      />
+    </Container>
+  );
+}
+
+export default App;
